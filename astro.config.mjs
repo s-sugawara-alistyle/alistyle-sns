5ce1a4ab81bfec6bb3930e342dafb3dcd5b87bbd
s.sugawara <s.sugawara@alistyle.jp>
2023-10-27 12:54:02 +0900

    修正

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,8 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: 'https://{s-sugawara-alistyle}.github.io/',
+    outDir: "./docs",
+    base: '/{alistyle-sns}'
+});
